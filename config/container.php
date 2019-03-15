c4da37858bc81f895e89cd0262d662b862bd7e37
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2019-03-15 19:35:40 +0700

    fixing service container and pattern

--- config/container.php
+++ config/container.php
@@ -6,19 +6,70 @@
 |--------------------------------------------------------------------------
 */
 
-$capsule = new \Illuminate\Database\Capsule\Manager();
+$capsule = new \Illuminate\Database\Capsule\Manager;
 $capsule->addConnection(config('db'));
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
+/**
+ * @return \Illuminate\Database\Capsule\Manager
+ */
 $container['db'] = function () use (&$capsule) {
     return $capsule;
 };
 
-$container['view'] = new App\Services\ViewService;
-$container['serializer'] = new App\Services\SerializerService;
-$container['validator'] = new \App\Services\ValidatorService;
-$container['logger'] = new App\Services\LoggerService;
+/**
+ * @return \Slim\Views\Twig
+ */
+$container['view'] = function () {
+    $view = new \Slim\Views\Twig(
+        config('view.template_path'),
+        config('view.twig')
+    );
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        container()->get('router'),
+        container()->get('request')->getUri()
+    ));
+
+    return $view;
+};
+
+/**
+ * @return \League\Fractal\Manager
+ */
+$container['serializer'] = function () {
+    $serializer = new \League\Fractal\Manager;
+    $serializer->setSerializer(new \App\Serializers\CustomSerializer);
+    $serializer->parseIncludes(request('include', []));
+
+    return $serializer;
+};
+
+/**
+ * @return \App\Validations\Validator
+ */
+$container['validator'] = function () {
+    return new \App\Validations\Validator;
+};
+
+/**
+ * @return \Monolog\Logger
+ */
+$container['logger'] = function () {
+    $logger = new \Monolog\Logger(config('logger.name'));
+    $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+
+    try {
+        $logger->pushHandler(
+            new \Monolog\Handler\StreamHandler(config('logger.path'),
+                \Monolog\Logger::DEBUG)
+        );
+    } catch (\Exception $exception) {
+
+    }
+
+    return $logger;
+};
 
 /*
 |--------------------------------------------------------------------------
